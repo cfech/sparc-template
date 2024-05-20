c28ff65e7c1f5166f77d52afc293227e63e37fa8
Connor Fech <cfech44@gmail.com>
2024-05-19 22:40:04 -0400

    updating gitignore and frontend build script

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Step 1: Remove the /frontend/dist folder
-echo "Removing /frontend/dist folder..."
+echo "Removing outdated resources"
+rm -rf ./frontend/dist
 rm -rf ./frontend/dist
 
 # Step 2: Rebuild the /frontend/dist folder with npm run build
@@ -10,8 +11,11 @@ cd ./frontend
 npm run build
 cd ..
 
+
+
 # Step 3: Copy the contents of the frontend/dist folder to the backend/static folder
 echo "Copying contents to /backend/static folder..."
+mkdir -p ./backend/static
 cp -r ./frontend/dist/* ./backend/static
 
 echo "Build process completed."
